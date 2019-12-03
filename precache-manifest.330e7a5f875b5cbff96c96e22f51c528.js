45a94ddce37d761960d8f3bf5f024f830150ab1a
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2019-12-03 09:22:10 +0300

    Updates

---
+++ precache-manifest.330e7a5f875b5cbff96c96e22f51c528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "e50a3c07a9d3a0f473ab",
+    "url": "/myapp/static/js/main.edb08811.chunk.js"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/myapp/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "e50a3c07a9d3a0f473ab",
+    "url": "/myapp/static/css/main.9212a6df.chunk.css"
+  },
+  {
+    "revision": "b90076d1da9fc81863c222ed2a0640b8",
+    "url": "/myapp/index.html"
+  }
+];
